bafc4e3734936a1dc83083afb0f993430dfc70d9
lorem.cookie <lorem-cookie@gmail.com>
2020-07-17 15:47:29 +0200

    Update lint script to use golangci-lint.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Check if golangci linter is installed
+if [[ ! -f $(go env GOPATH)/bin/golangci-lint ]]; then
+  # IF golangci linter is not installed install it
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
+fi
+
+# Print version of linter
+golangci-lint version
+
 # Make the modules nice and clean
 go mod tidy
 
-# Lint withe the gometalinter linter
-gometalinter ./... --vendor
+# Lint withe the golangci linter
+golangci-lint run cmd/... pkg/... internal/...
